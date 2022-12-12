9b4a904382fa98a31fb484040822a2413f8193a8
chutauruk <cyn.uruk@gmail.com>
2022-12-12 15:40:37 -0700

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 // Import Dependencies
 const express = require("express"); // import express
 
+// Database Connection
+const pokemon = require("./models/pokemon"); // import pokemon
+
 // Create our Express Application Object Bind Liquid Templating Engine
 const app = express();
+
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 
 // Routes
 ////////////////////////////////////////////
 app.get("/", (req, res) => {
     res.send("Welcome to the Pokemon App!");
   });
+
+  app.get ("/pokemon", (req, res) => {
+    res.send("pokemon")
+    
+})
+
 
 // Server Listener
 //////////////////////////////////////////////
